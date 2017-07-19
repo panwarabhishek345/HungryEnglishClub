2e838ab53771b22e56b7399058b29b45f06675c7
Vnnovate <mehul110@gmail.com>
2017-07-19 10:45:38 +0530

    Remove Teacher From List

---
+++ app/src/main/java/app/com/HungryEnglish/Activity/DemoActivity.java
@@ -0,0 +1,18 @@
+package app.com.HungryEnglish.Activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import app.com.HungryEnglish.R;
+
+/**
+ * Created by Vnnovate on 7/19/2017.
+ */
+
+public class DemoActivity extends  BaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
